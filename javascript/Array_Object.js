2777b01826351a78e626ca16edf8b43cb53fcaa3
callor <callor@callor.com>
2021-07-23 08:55:56 +0900

    JS_Array_Object

---
+++ javascript/Array_Object.js
@@ -0,0 +1,11 @@
+let playList = [];
+let s_row;
+addEventListener("click", (e) => {
+  let play = {
+    s_title: "제목",
+    s_singer: "조용필",
+    s_row: s_row++,
+  };
+  play.name = "홍기동";
+  plaLayList.push(play);
+});
